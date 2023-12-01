875a294922a5b13f3044f00ce075767122b9551f
Tazeela <headrick.d@hotmail.com>
2023-12-01 13:38:40 -0800

    Prepare for day 2 by adding files.

---
+++ AdventOfCode2023/Day2.cs
@@ -0,0 +1,22 @@
+﻿using AdventLib;
+
+namespace AdventOfCode2023;
+
+/// <summary>
+/// Solver for [day 2](https://adventofcode.com/2023/day/2)
+/// </summary>
+public class Day2 : AdventSolver {
+    public override string Day => "day2";
+
+    public override void Solve(string filename) {
+        int count1 = 0;
+        int count2 = 0;
+
+        foreach (String game in ReadLines(filename)) {
+            
+        }
+
+         Console.WriteLine("Solution for part 1 is: " + count1);
+         Console.WriteLine("Solution for part 2 is: " + count2);
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -2,7 +2,7 @@
 using AdventOfCode2023;
 
 Console.WriteLine("Hello, World 2023!");
-IEnumerable<AdventSolver> solvers = new AdventSolver[] { new Day1() };
+IEnumerable<AdventSolver> solvers = new AdventSolver[] { new Day1(), new Day2() };
 
 
 AdventRunner.ProcessRequests(solvers);
