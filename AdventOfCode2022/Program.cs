64a78be584e602ad17606a847d22cf5149c237b3
Tazeela <headrick.d@hotmail.com>
2023-11-30 18:41:58 -0800

    Add 2022 day 7

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -3,7 +3,7 @@ using AdventLib;
 using AdventOfCode2022;
 
 Console.WriteLine("Hello, World 2022!");
-IEnumerable<AdventSolver> solvers = new AdventSolver[] { new ElfFood(), new RockPaperScissors(), new RucksackReorg(), new CampCleanup(), new SupplyStacks(), new TuningTrouble() };
+IEnumerable<AdventSolver> solvers = new AdventSolver[] { new ElfFood(), new RockPaperScissors(), new RucksackReorg(), new CampCleanup(), new SupplyStacks(), new TuningTrouble(), new NoSpace() };
 
 
 AdventRunner.ProcessRequests(solvers);
